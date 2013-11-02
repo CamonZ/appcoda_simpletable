fe4120329350456aac62713d4eda7000985c96f4
Simon Garcia <simon.garciar@gmail.com>
2013-11-03 00:36:31 +0100

    save recipees as a Plist instead of hardcoded

--- SimpleTable/SimpleTableViewController.m
+++ SimpleTable/SimpleTableViewController.m
@@ -56,26 +56,14 @@
 - (void)viewDidLoad{
   [super viewDidLoad];
 	// Initialize table data
-  tableData = [NSArray arrayWithObjects:@"Egg Benedict",
-    @"Mushroom Risotto", @"Full Breakfast", @"Hamburger",
-    @"Ham and Egg Sandwich", @"Creme Brelee", @"White Chocolate Donut",
-    @"Starbucks Coffee", @"Vegetable Curry", @"Instant Noodle with Egg",
-    @"Noodle with BBQ Pork", @"Japanese Noodle with Pork", @"Green Tea",
-    @"Thai Shrimp Cake", @"Angry Birds Cake", @"Ham and Cheese Panini", nil];
   
-  thumbnails = [NSArray arrayWithObjects:@"egg_benedict.jpg",
-    @"mushroom_risotto.jpg", @"full_breakfast.jpg", @"hamburger.jpg",
-    @"ham_and_egg_sandwich.jpg", @"creme_brelee.jpg",
-    @"white_chocolate_donut.jpg", @"starbucks_coffee.jpg", @"vegetable_curry.jpg",
-    @"instant_noodle_with_egg.jpg", @"noodle_with_bbq_pork.jpg", @"japanese_noodle_with_pork.jpg",
-    @"green_tea.jpg", @"thai_shrimp_cake.jpg", @"angry_birds_cake.jpg", @"ham_and_cheese_panini.jpg", nil];
-
-  prepTimes = [NSArray arrayWithObjects:@"30 mins",
-    @"30 mins", @"20 mins", @"25 mins",
-    @"5 mins", @"20 mins",
-    @"10 mins", @"5 mins", @"15 mins",
-    @"10 mins", @"30 mins", @"30 mins",
-    @"2 mins", @"30 mins", @"30 mins", @"5 mins", nil];
+  NSString *path = [[NSBundle mainBundle] pathForResource:@"recipes" ofType:@"plist"];
+  
+  NSDictionary *dict = [[NSDictionary alloc] initWithContentsOfFile:path];
+  
+  tableData = [dict objectForKey:@"RecipeName"];
+  thumbnails = [dict objectForKey:@"Thumbnail"];
+  prepTimes = [dict objectForKey:@"PrepTime"];
 }
 
 - (void)didReceiveMemoryWarning{
